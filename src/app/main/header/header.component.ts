af920b89503007ea5c5913bee95ea6a883de661f
Your Name <your.email@example.com>
2025-08-20 21:40:22 +0530

    home page ui changes

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component, HostListener } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  menuOpen = false;
+  showDropdown = false;
+
+  ngOnInit(){
+    AOS.init();
+  }
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const navbar = document.querySelector('.main-nav');
@@ -15,4 +21,10 @@ export class HeaderComponent {
       navbar?.classList.remove('scrolled');
     }
   }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    setTimeout(() => AOS.refresh(), 100);
+  }
 }
+
